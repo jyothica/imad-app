43a11b4d72e2180cf134c613e16e32abf23bed3a
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-24 01:24:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var marginLeft = 0;
 function moveRight() {
     marginLeft = marginLeft + 1;
     img.style.marginLeft = marginLeft + 'px';
-}
+};
 img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     img.style.marginLeft = '100px';
@@ -18,15 +18,29 @@ img.onclick = function() {
 
 // Counter code
 var button = document.getElementById('counter');
-var counter=0;
+
 button.onclick = function() {
-    // make a request to the counter endpoint
-    
+    // create a request object
+        var request = new XMLHttpRequest();
+        
     // capture the response and store it in the variable
+    request.onreadystatechange = function() {
+       if(request.readyState == XMLHttpRequest.DONE) {
+           // take some action
+        if(request.status == 200) {
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }   
+       } 
+       //not done yet
+    };
     
-    // render the variable in the correct span
-    counter = counter+1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Make the request
+    request.open('GET', 'http://jyothicatauro.imad.hasura-app.io/', true);
+    request.send(null);
     
-}
+    
+    
+    
+};
