2c7b2f5dcdcc92498ed762817009e16b43c7a198
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-24 01:05:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,20 @@
+//console.log('Loaded!');
 
+/* change the text of the main-text div
+var element = document.getElementById('main-text');
+element.innerHTML = 'New Value';
+
+// move the image
+var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight() {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onclick = function() {
+    var interval = setInterval(moveRight, 50);
+    img.style.marginLeft = '100px';
+}; */
 
 // Counter code
 var button = document.getElementById('counter');
