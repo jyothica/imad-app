2114b0e2ac0fc85bc2a08140598ebd1c97a5572e
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-24 01:03:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,18 @@ img.onclick = function() {
     var interval = setInterval(moveRight, 50);
     img.style.marginLeft = '100px';
 };
+
+// Counter code
+var button = document.getElementById('counter');
+var counter=0;
+button.onclick = function() {
+    // make a request to the counter endpoint
+    
+    // capture the response and store it in the variable
+    
+    // render the variable in the correct span
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+}
