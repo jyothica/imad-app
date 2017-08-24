1001a078b3aeac52aacb04e8fa3c16bc285f0824
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-25 02:18:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,7 +63,7 @@ function createTemplate (data) {
             <p><textarea name="comment" cols="25" rows="8"></textarea>
             <br/>
             <input type="submit" value="Submit" id="sub_btn"></input>
-            <input type="reset"></input></p>
+            <input type="reset" value=""></input></p>
            
         </body>
     </html>`;
