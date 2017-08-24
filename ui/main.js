e29407aa7c9e9e2f96cd0a2105d7b06610f9460a
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-25 02:11:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,42 +79,13 @@ submit.onclick = function() {
   
   // ---------------------------------//
   
-  // Submit comment
+  // display comment
+  
+  var inputBox = document.getElementById('comment');
 
-var sub= document.getElementById('submit_btn');
-submit.onclick = function() {
-  
-      // create a request object
-        var request = new XMLHttpRequest();
-        
-    // capture the response and store it in a variable
-    request.onreadystatechange = function() {
-       if(request.readyState === XMLHttpRequest.DONE) {
-           // take some action
-        if(request.status === 200) {
-            // capture a list of name and render as a list
-              var names = request.responseText;
-              names = JSON.parse(names);
-              var list = '';
-              for (var i=0; i<names.length; i++) {
-                  list += '<li>'+ names[i] + '</li>';
-              }
-              var ul = document.getElementById('namelist');
-              ul.innerHTML = list;
-        }   
-       } 
-       //not done yet
-    };
-    
-    // Make the request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET', 'http://jyothicatauro.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
-  
-  // make request to the server and send the name
-  
-  
+inputBox.onkeyup = function(){
+    document.getElementById('printchatbox').innerHTML = inputBox.value;
+}
 };
 
 
