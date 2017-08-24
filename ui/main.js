42876ce051b78ca8a877a5b8ec82f4126176e33e
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-24 23:53:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,8 +43,7 @@ button.onclick = function() {
 };
 
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit= document.getElementById('submit_btn');
 submit.onclick = function() {
   
@@ -71,6 +70,8 @@ submit.onclick = function() {
     };
     
     // Make the request
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     request.open('GET', 'http://jyothicatauro.imad.hasura-app.io/submit-name?name='+name, true);
     request.send(null);
   
