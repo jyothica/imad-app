4a07b1248c55144f54fc7c87aee1bd0b249b4f7b
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-24 23:48:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -47,16 +47,36 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit= document.getElementById('submit_btn');
 submit.onclick = function() {
+  
+      // create a request object
+        var request = new XMLHttpRequest();
+        
+    // capture the response and store it in a variable
+    request.onreadystatechange = function() {
+       if(request.readyState === XMLHttpRequest.DONE) {
+           // take some action
+        if(request.status === 200) {
+            // capture a list of name and render as a list
+              var names = request.responseText;
+              names = JSON.parse(names);
+              var list = '';
+              for (var i=0; i<names.length; i++) {
+                  list += '<li>'+ names[i] + '</li>';
+              }
+              var ul = document.getElementById('namelist');
+              ul.innerHTML = list;
+        }   
+       } 
+       //not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://jyothicatauro.imad.hasura-app.io/submit-name?name='+name, true);
+    request.send(null);
+  
   // make request to the server and send the name
   
-  // capture a list of name and render as a list
-  var names = ['name1', 'name2', 'name3', 'name4'];
-  var list = '';
-  for (var i=0; i<names.length; i++) {
-      list += '<li>'+ names[i] + '</li>';
-  }
-  var ul = document.getElementById('namelist');
-  ul.innerHTML = list;
+  
 };
 
 
