d3397ace81954b64099813f85ef2166c75a5070f
jyothica <31218078+jyothica@users.noreply.github.com>
2017-08-25 01:44:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ function createTemplate (data) {
             </div>
             <h3>Comments</h3>
             You can place your comments here : 
-            <input type="text" id="comment" size="100" maxlength="100" placeholder="comment"></input>
+            <input type="text" id="comment" size="1000" maxlength="50" placeholder="comment"></input>
             <input type="submit" value="Submit" id="submit_btn"></input>
             <ul id="commentlist">
                 
